b0ad065fb2b53f261e6b1c5c280112c419038f51
Your Name <you@example.com>
2016-02-26 01:51:50 +0200

    added colorization and add-ons for bike display

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 (function () {
+    var colorChoice = {
+        'pink': '#ff358b',
+        'blue': '#2980b9',
+        'white': '#fff',
+        'gold': '#d0a825',
+        'silver': '#a69e9d',
+        'beige': '#d1dbbd'
+    };
+
     $('.block--selected').click(function () {
         $('.choice--color').addClass('active');
         $(this).addClass('is-hidden-desktop');
@@ -9,14 +18,35 @@
         $('.block--selected').attr('class', 'figure customize__figure color-block block--selected').addClass(targetColor);
         console.log(targetColor);
         $('.choice--color').removeClass('active');
+        $('.bike-display use').css('fill', colorChoice[targetColor]);
+        console.log(colorChoice[targetColor]);
     });
 
     $('.gender-block span').click(function () {
         if ($(this).hasClass('icon-boy')) {
-            $('.bike-display use').attr('xlink:href', 'img/bike-m.svg#bike');
+            $('.bike-display use').attr('xlink:href', '#bike-m');
         } else {
-            $('.bike-display use').attr('xlink:href', 'img/bike-f.svg#bike');
+            $('.bike-display use').attr('xlink:href', '#bike-f');
         }
-        console.log($('.bike-display use').attr('xlink:href'));
+    });
+
+    $('.addon-block span').click(function () {
+        $(this).closest('.addon-block').toggleClass('block--selected');
+        if ($('.addon-block').hasClass('block--selected')) {
+            if ($(this).hasClass('icon-cart')) {
+                $('#trunk').attr('class', 'colorize');
+            } else {
+                $('#basket').attr('class', 'colorize');
+            }
+        } else {
+            if ($(this).hasClass('icon-cart')) {
+                $('#trunk').attr('class', 'colorize removable');
+            } else {
+                $('#basket').attr('class', 'colorize removable');
+            }
+        }
+
+        console.log($('#trunk').attr('class'));
+        console.log($('.addon-block').attr('class'));
     });
 })();
